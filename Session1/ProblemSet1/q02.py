64f3286a66d7d8ea91e09468e2629966c1b0e359
Ishancodet <ishan2627@gmail.com>
2026-06-22 10:02:29 -0400

    Remove greeting function and example call
    Remove the greeting function and its call.

--- Session1/ProblemSet1/q02.py
+++ Session1/ProblemSet1/q02.py
@@ -18,6 +18,3 @@ Welcome to The Hundred Acre Wood Winnie the Pooh! My name is Christopher Robin.
 
 # TODO: write your solution here
 
-def greeting(name,place):
-    print(f"Welcome to The Hundred Acre Wood {name}! My {place} name is Christopher Robin.")
-greeting("Michael" , "CHD")
