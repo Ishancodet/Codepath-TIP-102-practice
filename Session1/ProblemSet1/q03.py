0e1747fb3368e36e69e5cca40e063e6eda23bada
Ishancodet <ishan2627@gmail.com>
2026-06-22 15:53:16 -0400

    Solution for q3

--- Session1/ProblemSet1/q03.py
+++ Session1/ProblemSet1/q03.py
@@ -20,3 +20,15 @@ Sorry! I don't know Piglet's catchphrase!
 """
 
 # TODO: write your solution here
+def print_catchphrase(character):
+    if character =="Pooh":
+        print("Oh,bother!")
+    elif character =="Tigger":
+        print("TTFN: Ta-ta for now!")
+    elif character =="Eeyore":
+        print("Thanks for noticing me.")
+    elif character =="Christopher Robin":
+        print("Silly old bear.")
+    else:
+        print(f"Sorry! I don't know {character}'s catchphrase!")
+print_catchphrase("ishan")
