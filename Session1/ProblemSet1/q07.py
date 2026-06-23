140db45bb2ceb0b4f55ceba5f44d6f2d8fc084ff
Ishancodet <ishan2627@gmail.com>
2026-06-23 14:53:19 -0400

    Solved q7

--- Session1/ProblemSet1/q07.py
+++ Session1/ProblemSet1/q07.py
@@ -20,3 +20,13 @@ Example Output:
 """
 
 # TODO: write your solution here
+def count_less_than(race_times,threshold):
+    count = 0
+    for time in race_times:
+       if time < threshold:
+           count += 1
+    return count 
+race_times = [1, 2, 3, 4, 5, 6]
+print(count_less_than(race_times,5))
+
+
