184368e738f8b439e3ae9a9120c4516bb0a302f2
Ishancodet <ishan2627@gmail.com>
2026-07-01 15:33:03 -0400

    Add nanana_batman function implementation
    Implement nanana_batman function to print 'na' repeated x times.

---
+++ Session1/ProblemSet2/q07.py
@@ -0,0 +1,20 @@
+"""
+Session 1 - Problem Set 2 - Problem 7: NaNaNa Batman!
+
+Write a function nanana_batman() that accepts an integer x and prints 
+the string "nanana batman!" where "na" is repeated x times. 
+Do not use the * operator.
+
+Example Usage:
+x = 6
+nanana_batman(x)
+
+x = 0
+nanana_batman(x)
+
+Example Output:
+"nananananana batman!"
+"batman!"
+"""
+
+# TODO: write your solution here
