937b31a68a028caf07dbd2c6be3836cbb1fbdee2
Ishancodet <ishan2627@gmail.com>
2026-07-01 13:03:21 -0400

    Add get_last function to retrieve last list i
    Implement get_last function to return last item or None.

---
+++ Session1/ProblemSet2/q04.py
@@ -0,0 +1,19 @@
+"""
+Session 1 - Problem Set 2 - Problem 4: Last
+
+Implement a function get_last() that accepts a list of items and returns 
+the last item in the list. If the list is empty, return None.
+
+Example Usage:
+items = ["spider man", "batman", "superman", "iron man", "wonder woman", "black adam"]
+get_last(items)
+
+items = []
+get_last(items)
+
+Example Output:
+"black adam"
+None
+"""
+
+# TODO: write your solution here
