0b515f6b125ce08557dfe94d4aeb31144b2a7939
Ishancodet <ishan2627@gmail.com>
2026-07-01 15:13:20 -0400

    Add squared function to square list of integers
    Implement the squared function to square a list of integers.

---
+++ Session1/ProblemSet2/q06.py
@@ -0,0 +1,15 @@
+"""
+Session 1 - Problem Set 2 - Problem 6: Squared
+
+Write a function squared() that accepts a list of integers numbers 
+as a parameter and squares each item in the list. Return the squared list.
+
+Example Usage:
+numbers = [1, 2, 3]
+squared(numbers)
+
+Example Output:
+[1, 4, 9]
+"""
+
+# TODO: write your solution here
