b2b6c821dd03ba0a3aba05ea073101c87929fc85
Ishancodet <ishan2627@gmail.com>
2026-07-01 12:57:16 -0400

    solved question 3

--- Session1/ProblemSet2/q03.py
+++ Session1/ProblemSet2/q03.py
@@ -21,3 +21,13 @@ Example Output:
 """
 
 # TODO: write your solution here
+def trilogy(year):
+    if year == 2005:
+        print("Batman Begins")
+    elif year ==2008:
+        print("The Dark Knight")
+    elif year == 2012:
+        print("The Dark Knight Rises")
+    else:
+        print(f"Christopher Nolan did not release a Batman movie in {year}.")
+trilogy(1993)
